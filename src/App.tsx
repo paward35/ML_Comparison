c372891e0ce9756d330a985bd4d82395af549bb1
Patrick Ward <patricksjward@gmail.com>
2024-03-20 19:27:34 +0100

    add basic website outline

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,27 @@ import Message from './message.tsx';
 
 
 function App() {
-  return <div><Message /></div>;
+  return <div>
+    <p>Put simply, WER is the ratio of errors in a transcript to the total words spoken. A lower WER in speech-to-text means better accuracy in recognizing speech. For example, a 20% WER means the transcript is 80% accurate. This website will compare the word error rate from different ASR vendors over time</p>
+    <h2>Batch ML</h2>
+    <p>From tests run on 1000 random sentences:</p>
+    <p>Running leaderboard</p>
+    <li>
+      Google: 100%
+    </li>
+    <li>
+      Amazon: 98%
+    </li>
+    <li>
+      Microsoft: 97%
+    </li>
+    <li>
+      Our Attempt: 101%
+    </li>
+    <h2>Continuous ML</h2>
+      Call (+1) 111-222-3333 to see your voice transcibed in real time
+    <h3>Live calls</h3>
+    </div>;
 }
 
 export default App; 
